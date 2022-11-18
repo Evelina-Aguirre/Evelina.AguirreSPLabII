a45c63c4adaff77604c02919741c26bf8faf786f
Eve <evelina.aguirre16@gmail.com>
2022-11-18 14:29:58 -0300

    Primer commit

---
+++ Evelina.aguirre.SPLaboII/Entidades/Lapiz.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Lapiz : Utiles
+    {
+        public Lapiz(int cantidadMaxima, List<Utiles> elementos) : base(cantidadMaxima, elementos)
+        {
+        }
+    }
+}
---
+++ Evelina.aguirre.SPLaboII/Entidades/Utiles.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Entidades
+{
+    public abstract class Utiles
+    {
+        private int cantidadMaxima;
+        private List<Utiles> elementos;
+
+        public Utiles(int cantidadMaxima, List<Utiles> elementos)
+        {
+                
+        }
+    }
+}
